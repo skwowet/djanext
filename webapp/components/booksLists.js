6aac9c8a3125a4ab245de484096caad67d4be69f
Yeganathan S <hilariousengineer@protonmail.com>
2021-11-05 20:06:08 +0530

    fixed the apolloclient issue

--- webapp/components/booksLists.js
+++ webapp/components/booksLists.js
@@ -21,7 +21,7 @@ function BooksList() {
 
   console.log("data", data.books);
 
-  return <h2>Helo world!</h2>;
+  return <h2>Helo {data.books[0].title}</h2>;
 }
 
 export default BooksList;
--- webapp/pages/_app.js
+++ webapp/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
+import BooksList from "../components/booksLists";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <div style={{ margin: "20px" }}>
+        <Component {...pageProps} />
+        <BooksList />
+      </div>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
